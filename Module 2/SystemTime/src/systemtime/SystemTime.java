2264edb3b2351dd0f9dc8e8239f96482d1e2c0b1
TranDung147 <dungttde180631@fpt.edu.vn>
2024-06-16 23:34:12 +0700

    add SystemTime

---
+++ Module 2/SystemTime/src/systemtime/SystemTime.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package systemtime;
+
+import java.util.Date;
+
+/**
+ *
+ * @author PC STUDENT
+ */
+public class SystemTime {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Date now = new Date();
+        
+        System.out.println("Now is: " + now);
+    }
+    
+}
