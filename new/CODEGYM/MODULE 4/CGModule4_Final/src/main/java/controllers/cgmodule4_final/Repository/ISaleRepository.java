7c8cd1ddece37e4c915a8bf5dd32c4164c35ff32
TranDung147 <155507424+TranDung147@users.noreply.github.com>
2025-01-20 22:03:17 +0700

    Update ISaleRepository.java
    a

--- new/CODEGYM/MODULE 4/CGModule4_Final/src/main/java/controllers/cgmodule4_final/Repository/ISaleRepository.java
+++ new/CODEGYM/MODULE 4/CGModule4_Final/src/main/java/controllers/cgmodule4_final/Repository/ISaleRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public interface ISaleRepository extends JpaRepository<Sale, Integer> {
@@ -12,4 +13,11 @@ public interface ISaleRepository extends JpaRepository<Sale, Integer> {
     List<Sale> getAllBySaleId();
     @Query("SELECT s FROM Sale s WHERE s.saleId = :id")
     Sale getSaleById(@Param("id") Integer id);
+    @Query("SELECT s FROM Sale s WHERE " +
+            "(:discount IS NULL OR s.discount >= :discount) AND " +
+            "(:beginDate IS NULL OR s.beginDate >= :beginDate) AND " +
+            "(:endDate IS NULL OR s.endDate <= :endDate)")
+    List<Sale> findByConditions(@Param("discount") Long discount,
+                                @Param("beginDate") LocalDate beginDate,
+                                @Param("endDate") LocalDate endDate);
 }
